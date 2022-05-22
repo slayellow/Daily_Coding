4f70fce68ccb9e5dace607752c0f87bbf051db1d
홍진석 <jinseokhong@MacBook-Pro.local>
2022-05-22 20:53:08 +0900

    [20220522] CleanCode ~p.82

--- cleancode/container_object.py
+++ cleancode/container_object.py
@@ -8,13 +8,13 @@ class Boundaries:
     def __contain__(self, coord):
         x, y = coord
         return 0 <= x < self.width and 0 <= y < self.height
-    
+
 
 class Grid:
     def __init__(self, width, height) -> None:
         self.width = width
         self.height = height
         self.limits = Boundaries(width, height)
-    
+
     def __contain__(self, coord):
         return coord in self.limits
